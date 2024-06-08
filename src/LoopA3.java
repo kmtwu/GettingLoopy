a333351285b486b01688e4981cdba999cc0e42e7
kmtwu <mr.kwu13579@gmail.com>
2024-06-08 11:06:59 -0400

    p3

---
+++ src/LoopA3.java
@@ -0,0 +1,10 @@
+public class LoopA3 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for (i = 0; i <= 6; i++) {
+            j = 3 * i;
+            System.out.println(j);
+        }
+    }
+}
