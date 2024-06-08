1aa682bb586eb73f23a86db1b1a1d5e254990e02
kmtwu <mr.kwu13579@gmail.com>
2024-06-08 11:08:50 -0400

    p4

---
+++ src/LoopA4.java
@@ -0,0 +1,10 @@
+public class LoopA4 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for (i = 5; i >= 0; i--) {
+            j = 2 * i;
+            System.out.println(j);
+        }
+    }
+}
