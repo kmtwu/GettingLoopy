e24624725c76a66cb59fb273517dbebc9320cc43
kmtwu <mr.kwu13579@gmail.com>
2024-06-08 15:05:45 -0400

    p5

---
+++ src/LoopB5.java
@@ -0,0 +1,17 @@
+public class LoopB5 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= 5; j++) {
+                if (i >= j) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
